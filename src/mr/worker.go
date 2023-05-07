cdd9d326376444ff5447027f2896dec5ec39534b
ShirishaSRao <31768648+ShirishaSRao@users.noreply.github.com>
2023-05-07 19:30:04 -0400

    Update worker.go
    rpc change

--- src/mr/worker.go
+++ src/mr/worker.go
@@ -216,9 +216,9 @@ func CallExample() {
 // returns false if something goes wrong.
 func call(rpcname string, args interface{}, reply interface{}) bool {
 	// c, err := rpc.DialHTTP("tcp", "127.0.0.1"+":1234")
-	sockname := coordinatorSock()
-	c, err := rpc.DialHTTP("unix", sockname)
-	// c, err := rpc.DialHTTP("tcp", "128.6.13.146:1234")
+	//sockname := coordinatorSock()
+	//c, err := rpc.DialHTTP("unix", sockname)
+	c, err := rpc.DialHTTP("tcp", "128.6.13.146:1234")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
